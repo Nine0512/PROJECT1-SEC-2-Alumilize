6bb6b79b91c2f77c160efcc9885b86323b7f7ce1
nonybueno <thaninpong.non@mail.kmutt.ac.th>
2023-02-07 12:44:41 +0700

    add random word

---
+++ random-word/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+const words = [];
+
+async function getRandomWord() {
+  const response = await fetch("https://random-word-api.herokuapp.com/word?number=20");
+  const data = await response.json();
+  return data;
+}
+
+async function getAndLogRandomWords() {
+    const word = await getRandomWord();
+    words.push(word);
+    console.log(word);
+}
+
+getAndLogRandomWords();
+
+
+</script>
+ 
+<template>
+<div>
+  
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
