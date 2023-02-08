4ff5dd25f99f20a7be0daf39d83da5a5b4432827
nonybueno <thaninpong.non@mail.kmutt.ac.th>
2023-02-08 15:28:34 +0700

    edit random

--- random-word/src/App.vue
+++ random-word/src/App.vue
@@ -2,16 +2,18 @@
 import { ref } from 'vue';
 
 
-let allWords = [];
+let allWords = []
+let words = []
+let showWords = ref('')
+let num = 20
+let index = ref(0)
+let countIndex = ref(0)
 
 async function getRandomWord() {
   const response = await fetch("https://random-word-api.herokuapp.com/word?number=1000");
   const data = await response.json();
   return data;
 }
-
-let words = ref([])
-let num = 20
 
 async function getAndLogRandomWords() {
     const word = await getRandomWord()
@@ -22,20 +24,46 @@ async function getAndLogRandomWords() {
 getAndLogRandomWords()
 
 function useWord(num){
-    words.value.length=0
+    words.length=0
     for (let i = 0; i < num; i++) {
       let ranWord = allWords[Math.floor(Math.random() * allWords.length)]
-      words.value.push(ranWord)
+      words.push(ranWord)
     }
+    console.log(words)
+    showWords.value = words.join(' ')
 }
 
-  </script>
-  
+// benz
+window.addEventListener( 'keydown',  event => {
+if( event.key === words.join(' ').charAt( index.value ) ){
+    console.log('correct');
+    index.value++;
+   
+} else  {
+    console.log('wrong');
+   
+} 
+countIndex.value++;
+if( words.length === index.value ){
+    console.log('end');
+}
+})
+
+
+</script>
   <template>
-  <div>
+<div>
     <button @click="useWord(num)" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Get Random Word</button>
-    <p> {{ words }}</p>
+  <div>
+  <p>{{ showWords }}</p>
   </div>
+
+  <div>
+  <p>index : {{  index }}</p>
+  </div>
+
+  <p>countIndex : {{ countIndex }}</p>
+</div>
   </template>
  
 <style scoped>
