e93b75744bf13eb7b1f008cbbe9d0b4b5cba3c84
Worawit <winwin14.lt@gmail.com>
2023-02-12 17:39:37 +0700

    again

--- PROJECT-sec-2-Alumilize/src/App.vue
+++ PROJECT-sec-2-Alumilize/src/App.vue
@@ -6,26 +6,35 @@ let score = ref(0)
 let timer = ref(30)
 
 let func = (event) => {
-    if(score.value <= 0){score.value === 0}
+    
     const key = event.key;
     console.log(key);
     // blick.value+=key //ใส่+=เพราะเมื่อรับค่าจะคีย์บอร์ดจะใส่แค่เท่ากับมันจะเปลี่ยนไปเรื่อยๆแต่ไม่รับค่า
     if (key !== 'Backspace') {blick.value+=key}
-    if(key === 'Backspace'){blick.value = blick.value.slice(0,-1)}
+    if(key === 'Backspace'){
+      blick.value = blick.value.slice(0,-1)
+      score.value -= 1
+    }
     // if(key === 'Backspace'){score.value -= 1}
-    for (let i = 0; i <= text.length; i++) {
-      if(blick.value === text[i]){
-        score.value +=1
-      }
+    if(key === text.charAt(score.value)){
+      console.log('correct');
+      score.value++
+    }else{
+      console.log('incorrect');
+    }
+    if(blick.value === text){
+      clearInterval(interval)
+      window.removeEventListener('keydown',func)  
     }
     // if(blick.value === text[0]){score.value +=1}
     
 }
+
 const block = window.addEventListener('keydown',func)
 let interval = setInterval(() => {
   if (timer.value === 0) {
     clearInterval(interval)
-    // window.removeEventListener('keydown',func)                
+    window.removeEventListener('keydown',func)                
   } else {
     timer.value--
     console.log(timer)
